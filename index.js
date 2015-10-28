2f9be0c999c1bf5e342171fb1ff10d2f2ae72b60
Boryslav Larin <brabadu@gmail.com>
2015-10-29 01:17:42 +0200

    Working with effects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-import StartApp from 'simple';
+import React from 'react';
+import {render} from 'react-dom';
+import Rx from 'rx';
+import {actionIs} from './utils.js';
+
+
+/*
+update is an Array of trios: [actionName, actionHandler, effect]
+*/
+
+export default function(model, update, View, container) {
+  let eventStream = new Rx.Subject();
+
+  let dispatcherArray = update.map(([actionName, actionHandler]) =>
+    eventStream.filter(actionIs(actionName)).map(actionHandler))
+
+  return Rx.Observable
+    .merge(...dispatcherArray)
+    .scan(((state, action) => action(state)), model)
+    .startWith([model])
+    .do(([state, _]) => render(<View {...state} eventStream={eventStream} />, container))
+    .flatMap(([state, effect]) => effect ? effect(state, eventStream) : Rx.Observable.empty() )
+    .subscribe(
+      function(){},
+      console.error.bind(console)
+    );
+}
