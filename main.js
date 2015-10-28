f939df2e65252c4d400970ca7e9165582be34c45
Boryslav Larin <brabadu@gmail.com>
2015-10-29 01:22:32 +0200

    Example usage of tanok

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-import StartApp from './simple.js';
+import StartApp from './tanok.js';
 import React from 'react'
 
 let model = {count: 0};
 let update = [
   ['inc', (params) => (state) => {
     state.count += 1
-    return state
+    return [state, wowEffect]
   }],
   ['dec', (params) => (state) => {
     state.count -= 1
-    return state
+    return [state]
+  }],
+  ['wow', (params) => (state) => {
+    state.count = 1000000
+    return [state]
   }]
 ]
 
+function wowEffect (state, eventStream) {
+  return Rx.Observable.fromCallback(setTimeout)(function(){
+    eventStream.onNext({
+      action: 'wow'
+    })
+  }, 3000)
+}
 
 const Counter = React.createClass({
   onPlusClick: function() {
