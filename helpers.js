adcd45dbb2a0b53528e89c2d8db65805f1ba2162
Dima Kruk <krkd@gmail.com>
2015-11-03 01:24:26 +0200

    proposal for wrapper over RxJs methods

--- helpers.js
+++ helpers.js
@@ -1,11 +1,15 @@
-export function actionIs(actionName) {
-  return function() {return this.filter(({action}) => action === actionName)};
-}
+"use strict";
 
-export function filter(cond) {
-  return function() {return this.filter(cond)}
-}
+var _ = methodName => ( (...args) => {
+    var args = args;
+    return function() {return this[methodName].apply(this, args)}
+})
 
-export function debounce(time) {
-  return function() { return this.debounce(time)}
-}
+export var filter = _('filter');
+
+export var debounce = _('debounce');
+
+export var actionIs = actionName => (
+    function() { return filter(({action}) => action === actionName).call(this)}
+);
+
