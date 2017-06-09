655231da2ea7c515c46b5e71374405e751f2460c
Boryslav Larin <brabadu@gmail.com>
2017-06-10 00:34:59 +0300

    Updated examples a bit

--- examples/main.js
+++ examples/main.js
@@ -19,16 +19,9 @@ import {
   TwoCounters,
 } from './3_subcomponents/subcomponents.js';
 
-
 import {
   init as init_4,
   Dashboard as Dashboard2,
-  CountersCollection,
-} from './4_subcomponent_collection/subcomponents.js';
-
-import {
-  init as init_5,
-  Dashboard as Dashboard3,
   TwoColumns,
 } from './4_subcomponent_collection/two_columns.js';
 
@@ -36,24 +29,33 @@ import { loggingMiddleware } from './7_middlewares/middlewares'
 
 // basic usage
 tanok(init_1(), (new CounterDispatcher1), Counter1, {
+  container: document.getElementById('basics'),
   middlewares: [loggingMiddleware(1), loggingMiddleware(2)],
 });
 
 // Using effects (asynchronous events)
-tanok(init_2(), (new CounterDispatcher2), Counter2);
+tanok(init_2(), (new CounterDispatcher2), Counter2, {
+  container: document.getElementById('effects'),
+});
 
 // Simple subcomponents
-tanok(init_3(), (new Dashboard), TwoCounters);
+tanok(init_3(), (new Dashboard), TwoCounters, {
+  container: document.getElementById('subcomponents'),
+});
 
 // Subcomponents for handling collection of subitems
-tanok(init_4(), (new Dashboard2), CountersCollection);
-tanok(init_5(), new Dashboard3, TwoColumns);
+tanok(init_4(), (new Dashboard2), TwoColumns, {
+  container: document.getElementById('sub-collection'),
+});
 
 // Outer event stream example
 import Rx from 'rx';
 
-const ticks = Rx.Observable.interval(1000).map(() => { return {
+const ticks = Rx.Observable.interval(1000).map(() => ({
   streamName: null,
   action: 'inc'
-}});
-tanok(init_1(), (new CounterDispatcher1), Counter1, {outerEventStream: ticks});
+}));
+tanok(init_1(), (new CounterDispatcher1), Counter1, {
+  container: document.getElementById('outer-event-stream'),
+  outerEventStream: ticks
+});
